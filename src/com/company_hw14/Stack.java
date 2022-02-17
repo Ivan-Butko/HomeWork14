26d816d55e36c952fb50eaff75786f4e2f7e41a7
Ivan-Butko <96598227+Ivan-Butko@users.noreply.github.com>
2022-02-17 06:55:45 +0200

    Init Commit New: bag del (indexColor -> index)

---
+++ src/com/company_hw14/Stack.java
@@ -0,0 +1,7 @@
+package com.company_hw14;
+
+public interface Stack<T> {
+    void push(T element);
+    T pop();
+    T peek();
+}
--- src/com/company_hw14/Warehouse.java
+++ src/com/company_hw14/Warehouse.java
@@ -1,5 +1,27 @@
 package com.company_hw14;
 
-public class Warehouse {
+public class Warehouse implements Stack<Box>{
+    private Box[] data;
 
+    public Warehouse() {
+    }
+
+    public Warehouse(Box box) {
+
+    }
+
+    @Override
+    public void push(Box element) {
+
+    }
+
+    @Override
+    public Box pop() {
+        return null;
+    }
+
+    @Override
+    public Box peek() {
+        return null;
+    }
 }
